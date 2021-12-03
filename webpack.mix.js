2a4c1a68107b9ad4db7e8e7667761219712bc0a7
Fahmi Jabbar <rootmawar77@gmail.com>
2021-12-04 01:26:36 +0700

    Reconfigure laravel mix, use scss & babel.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
+mix.disableSuccessNotifications()
+    .js("resources/js/app.js", "public/js")
+    .babel("public/js/app.js", "public/js/app.es5.js")
     .sass("resources/sass/app.scss", "public/css")
     .version();
