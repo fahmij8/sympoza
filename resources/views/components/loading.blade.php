689fe0175a0ac7faf8ad4115591cf450a839a6be
Fahmi Jabbar <rootmawar77@gmail.com>
2021-12-04 01:28:33 +0700

    Create loading components

---
+++ resources/views/components/loading.blade.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="loading-bg col d-flex justify-content-center align-items-center">
+        <div class="lds-roller d-block mx-auto">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
