2583ac1817c930c70c78586b2e61638538905e9c
Fahmi Jabbar <rootmawar77@gmail.com>
2021-12-26 16:25:12 +0700

    Fix model name

--- app/Models/Manuscript_Sympozia.php
+++ app/Models/Manuscript_Sympozia.php
@@ -2,6 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Conferences_Sympozia;
+use App\Models\ManuscriptAuthor_Sympozia;
+use App\Models\ManuscriptFile_Sympozia;
+use App\Models\ManuscriptMilestone_Sympozia;
+use App\Models\ManuscriptStatus_Sympozia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,26 +18,26 @@ class Manuscript_Sympozia extends Model
 
     public function file()
     {
-        return $this->hasMany(ManuscriptFile_sympozia::class, 'manuscript_id', 'id');
+        return $this->hasMany(ManuscriptFile_Sympozia::class, 'manuscript_id', 'id');
     }
 
     public function authors()
     {
-        return $this->hasOne(ManuscriptAuthor_sympozia::class, 'manuscript_id', 'id');
+        return $this->hasOne(ManuscriptAuthor_Sympozia::class, 'manuscript_id', 'id');
     }
 
     public function status()
     {
-        return $this->hasOne(ManuscriptStatus_sympozia::class, 'id', 'status_id');
+        return $this->hasOne(ManuscriptStatus_Sympozia::class, 'id', 'status_id');
     }
 
     public function milestone()
     {
-        return $this->hasOne(ManuscriptMilestone_sympozia::class, 'id', 'milestone_id');
+        return $this->hasOne(ManuscriptMilestone_Sympozia::class, 'id', 'milestone_id');
     }
 
     public function conferences()
     {
-        return $this->hasOne(Conferences_sympozia::class, 'id', 'conferences_id');
+        return $this->hasOne(Conferences_Sympozia::class, 'id', 'conferences_id');
     }
 }
